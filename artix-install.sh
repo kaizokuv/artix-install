9ecc30390ecd9cf0f390184a1856fd84270a2ace
feribsd <feri_kladek@protonmail.com>
2026-02-26 08:10:25 +0100

    Refactor artix-install.sh for improved clarity and flow

--- artix-install.sh
+++ artix-install.sh
@@ -1,87 +1,108 @@
 #!/bin/bash
-set -e 
 
-# --- PRE-FLIGHT ---
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root."
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Run as root."
     exit 1
 fi
 
-# --- NETWORK CHECK ---
-if ! ping -c 1 artixlinux.org &>/dev/null; then
-    whiptail --title "Network Offline" --msgbox "Launching nmtui to connect..." 10 60
-    nmtui
-    ping -c 1 artixlinux.org &>/dev/null || { echo "Offline. Exiting."; exit 1; }
-fi
+clear
 
-# --- USER INPUT ---
-DISK=$(lsblk -dpno NAME,SIZE | grep -E "/dev/sd|/dev/nvme" | \
-whiptail --menu "Select installation disk" 20 80 10 \
-$(lsblk -dpno NAME,SIZE | grep -E "/dev/sd|/dev/nvme") 3>&1 1>&2 2>&3)
+TITLE="Artix Linux Installer"
 
-[[ -z "$DISK" ]] && exit 1
-SWAPSIZE=$(whiptail --inputbox "Enter swap size (e.g., 8G):" 10 60 "8G" 3>&1 1>&2 2>&3)
-USERNAME=$(whiptail --inputbox "Enter username:" 10 60 "user" 3>&1 1>&2 2>&3)
-PASSWORD=$(whiptail --passwordbox "Enter password for root and $USERNAME:" 10 60 3>&1 1>&2 2>&3)
+DISK=$(lsblk -dpnoNAME,SIZE | grep -v loop | whiptail \
+--title "$TITLE" \
+--menu "Select installation disk" 20 70 10 \
+$(lsblk -dpnoNAME,SIZE | grep -v loop | awk '{print $1 " " $2}') \
+3>&1 1>&2 2>&3)
 
-# --- CLEANUP & PARTITIONING ---
-swapoff -a || true
+[ -z "$DISK" ] && exit
+
+whiptail --title "$TITLE" --yesno "ALL DATA ON $DISK WILL BE DESTROYED" 10 60 || exit
+
 umount -R /mnt 2>/dev/null || true
-wipefs -af "$DISK"
-dd if=/dev/zero of="$DISK" bs=1M count=1 conv=notrunc
-# Create partitions
-printf "label: gpt\n,1G,U\n,%s,S\n,,L\n" "$SWAPSIZE" | sfdisk --force "$DISK"
-blockdev --rereadpt "$DISK" || true
-udevadm settle && sleep 2
+swapoff -a 2>/dev/null || true
 
-# Identify partitions
-[[ "$DISK" == *"nvme"* ]] && P="p" || P=""
-EFI="${DISK}${P}1"; SWAP="${DISK}${P}2"; ROOT="${DISK}${P}3"
+sgdisk --zap-all "$DISK"
 
-# --- FORMAT & MOUNT ---
+sgdisk -n 1:0:+512M -t 1:ef00 "$DISK"
+sgdisk -n 2:0:0 -t 2:8300 "$DISK"
+
+EFI="${DISK}1"
+ROOT="${DISK}2"
+
 mkfs.fat -F32 "$EFI"
-mkswap -f "$SWAP" && swapon "$SWAP"
-mkfs.xfs -f "$ROOT"
+mkfs.ext4 -F "$ROOT"
+
 mount "$ROOT" /mnt
-mkdir -p /mnt/boot && mount "$EFI" /mnt/boot
+mkdir -p /mnt/boot
+mount "$EFI" /mnt/boot
 
-# --- BASESTRAP + RECOMMENDED APPS ---
-# Removed 'sudo', added 'opendoas' and essentials
-echo "Installing base system and essential apps..."
-pacman -Sy --noconfirm artix-keyring archlinux-keyring || true
-
-basestrap /mnt base base-devel dinit elogind-dinit linux-zen linux-firmware \
-intel-ucode grub efibootmgr networkmanager-dinit dbus-dinit opendoas \
-htop nvtop neovim fastfetch git bat lsd tldr \
-pipewire pipewire-pulse wireplumber alsa-utils
+basestrap /mnt \
+base base-devel \
+linux linux-firmware \
+dinit elogind-dinit \
+doas vi \
+networkmanager networkmanager-dinit \
+pipewire pipewire-alsa pipewire-pulse wireplumber \
+zramen \
+grub efibootmgr \
+ntfs-3g dosfstools mtools \
+whiptail
 
 fstabgen -U /mnt >> /mnt/etc/fstab
 
-# --- CHROOT CONFIG ---
-artix-chroot /mnt /bin/bash <<EOF
-set -e
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
-grub-mkconfig -o /boot/grub/grub.cfg
+LOCALE=$(whiptail \
+--title "$TITLE" \
+--menu "Select locale" 20 70 10 \
+$(grep "UTF-8" /mnt/usr/share/i18n/SUPPORTED | head -20 | awk '{print $1 " " ""}') \
+3>&1 1>&2 2>&3)
 
-mkdir -p /etc/dinit.d/boot.d
-for svc in dbus elogind NetworkManager; do
-    ln -sf /etc/dinit.d/\$svc /etc/dinit.d/boot.d/\$svc
-done
+echo "$LOCALE UTF-8" >> /mnt/etc/locale.gen
 
-# DOAS CONFIG (The Sudo Killer)
-echo "root:$PASSWORD" | chpasswd
-useradd -m -G wheel -s /bin/bash "$USERNAME"
-echo "$USERNAME:$PASSWORD" | chpasswd
-echo 'permit :wheel' > /etc/doas.conf
-# Optional: Alias doas to sudo for muscle memory
-echo "alias sudo='doas'" >> /home/$USERNAME/.bashrc
-EOF
+arch-chroot /mnt locale-gen
 
-# --- WRAP UP ---
-umount -R /mnt
-sync
-if whiptail --yesno "Installation complete. Reboot now?" 10 60; then
+echo "LANG=$LOCALE" > /mnt/etc/locale.conf
+
+ln -sf /usr/share/zoneinfo/Europe/Bratislava /mnt/etc/localtime
+arch-chroot /mnt hwclock --systohc
+
+HOSTNAME=$(whiptail \
+--title "$TITLE" \
+--inputbox "Enter hostname" 10 60 artix \
+3>&1 1>&2 2>&3)
+
+echo "$HOSTNAME" > /mnt/etc/hostname
+
+arch-chroot /mnt passwd
+
+USERNAME=$(whiptail \
+--title "$TITLE" \
+--inputbox "Enter username" 10 60 user \
+3>&1 1>&2 2>&3)
+
+arch-chroot /mnt useradd -m -G wheel,audio,video,storage "$USERNAME"
+
+arch-chroot /mnt passwd "$USERNAME"
+
+echo "permit persist :wheel" > /mnt/etc/doas.conf
+
+arch-chroot /mnt chown root:root /etc/doas.conf
+arch-chroot /mnt chmod 0400 /etc/doas.conf
+
+arch-chroot /mnt ln -s /etc/dinit.d/networkmanager /etc/dinit.d/boot.d/
+
+arch-chroot /mnt ln -s /etc/dinit.d/elogind /etc/dinit.d/boot.d/
+
+arch-chroot /mnt ln -s /etc/dinit.d/zramen /etc/dinit.d/boot.d/
+
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Artix
+
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+whiptail --title "$TITLE" --yesno "Installation complete. Reboot?" 10 60
+
+if [ $? -eq 0 ]; then
     reboot
-else
-    echo "You chose not to reboot. You may reboot manually."
 fi
