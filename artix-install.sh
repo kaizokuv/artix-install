4560908c5da9f1f41e9c8f738fb561766bbe9762
feribsd <feri_kladek@protonmail.com>
2026-02-26 11:05:35 +0100

    Update artix-install.sh

--- artix-install.sh
+++ artix-install.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 set -e
 set -o pipefail
@@ -30,59 +29,26 @@ validate_input() {
     echo "$input"
 }
 
-# 1. Choose Desktop Environment / WM / Shell
-UI_TYPE=$(whiptail --title "$TITLE" --menu "Choose interface type:" 20 60 3 \
-"DE" "Desktop Environment" \
-"WM" "Window Manager" \
-"SHELL" "CLI Shell Only" 3>&1 1>&2 2>&3)
-validate_input "$UI_TYPE"
-
-if [[ "$UI_TYPE" == "DE" ]]; then
-    DE_CHOICE=$(whiptail --title "$TITLE" --menu "Select Desktop Environment:" 20 70 5 \
-    "Plasma" "KDE Plasma Full Suite" \
-    "XFCE" "XFCE4 + Goodies" \
-    "MATE" "MATE Desktop + Extras" \
-    "LXQt" "LXQt Desktop" \
-    "Moksha" "Moksha Desktop (Enlightenment fork)" 3>&1 1>&2 2>&3)
-    validate_input "$DE_CHOICE"
-elif [[ "$UI_TYPE" == "WM" ]]; then
-    WM_CHOICE=$(whiptail --title "$TITLE" --menu "Select Window Manager:" 20 60 3 \
-    "i3" "i3 Tiling WM" \
-    "xmonad" "xmonad Tiling WM" \
-    "WindowMaker" "Window Maker" 3>&1 1>&2 2>&3)
-    validate_input "$WM_CHOICE"
-fi
-
-# 2. Disk Selection
 DISK=$(lsblk -dpnoNAME,SIZE | grep -v loop | whiptail \
 --title "$TITLE" \
 --menu "Select installation disk (Use arrows/type first letter)" 20 70 10 \
-$(lsblk -dpnoNAME,SIZE | grep -v loop | awk '{print $1 " " $2}') 3>&1 1>&2 2>&3)
-validate_input "$DISK"
+$(lsblk -dpnoNAME,SIZE | grep -v loop | awk '{print $1 " " $2}') \
+3>&1 1>&2 2>&3)
+
+[ -z "$DISK" ] && exit 1
 
 whiptail --title "$TITLE" --yesno "ALL DATA ON $DISK WILL BE DESTROYED" 10 60 || exit 1
 
-# 3. Filesystem & Swap
-FS_CHOICE=$(whiptail --title "$TITLE" --menu "Select Root File System:" 15 60 4 \
-"ext4" "Ext4 Standard" \
-"btrfs" "Btrfs" \
-"xfs" "XFS" \
-"f2fs" "F2FS" 3>&1 1>&2 2>&3)
-validate_input "$FS_CHOICE"
+# Root FS and Swap selection
+FS_CHOICE=$(whiptail --title "$TITLE" --menu "Select Root File System" 15 60 4 \
+"ext4" "Standard Ext4" \
+"btrfs" "B-Tree Filesystem" \
+"xfs" "XFS Filesystem" \
+"f2fs" "Flash-Friendly Filesystem" 3>&1 1>&2 2>&3)
 
-SWAPSIZE=$(whiptail --title "$TITLE" --inputbox "Enter swap size (e.g., 8G):" 10 60 "8G" 3>&1 1>&2 2>&3)
-validate_input "$SWAPSIZE"
+SWAPSIZE=$(whiptail --title "$TITLE" --inputbox "Enter swap size (e.g., 8G or 4096M)" 10 60 "8G" 3>&1 1>&2 2>&3)
 
-# 4. User Info
-HOSTNAME=$(whiptail --title "$TITLE" --inputbox "Enter hostname:" 10 60 artix 3>&1 1>&2 2>&3)
-validate_input "$HOSTNAME"
-
-ROOT_PW=$(get_password "Enter root password:")
-USERNAME=$(whiptail --title "$TITLE" --inputbox "Enter username:" 10 60 user 3>&1 1>&2 2>&3)
-validate_input "$USERNAME"
-USER_PW=$(get_password "Enter password for $USERNAME:")
-
-# --- Partitioning ---
+# Cleanup & partition
 umount -R /mnt 2>/dev/null || true
 swapoff -a 2>/dev/null || true
 wipefs -af "$DISK"
@@ -104,92 +70,139 @@ EOF
 
 udevadm settle
 sleep 2
+
 [[ "$DISK" =~ [0-9]$ ]] && P="p" || P=""
 EFI="${DISK}${P}1"
 ROOT="${DISK}${P}2"
 
 mkfs.fat -F32 "$EFI"
 case $FS_CHOICE in
-    ext4)  mkfs.ext4 -F "$ROOT" ;;
+    ext4) mkfs.ext4 -F "$ROOT" ;;
     btrfs) mkfs.btrfs -f "$ROOT" ;;
-    xfs)   mkfs.xfs -f "$ROOT" ;;
-    f2fs)  mkfs.f2fs -f "$ROOT" ;;
+    xfs) mkfs.xfs -f "$ROOT" ;;
+    f2fs) mkfs.f2fs -f "$ROOT" ;;
 esac
 
 mount "$ROOT" /mnt
-mkdir -p /mnt/boot && mount "$EFI" /mnt/boot
+mkdir -p /mnt/boot
+mount "$EFI" /mnt/boot
 
-# --- Swap ---
-if [[ "$SWAPSIZE" != "0" ]]; then
-    dd if=/dev/zero of=/mnt/swapfile bs=1M count=$(echo "$SWAPSIZE" | sed 's/G/*1024/' | bc) status=progress
+# Swapfile parsing
+SWAP_MB=0
+if [[ "$SWAPSIZE" =~ G$ ]]; then
+    SWAP_MB=$(( ${SWAPSIZE%G} * 1024 ))
+elif [[ "$SWAPSIZE" =~ M$ ]]; then
+    SWAP_MB=${SWAPSIZE%M}
+else
+    whiptail --title "$TITLE" --msgbox "Invalid swap size format. Use e.g., 8G or 4096M." 10 60
+    exit 1
+fi
+
+if [[ "$SWAP_MB" -gt 0 ]]; then
+    echo "Creating ${SWAP_MB}MB swapfile..."
+    dd if=/dev/zero of=/mnt/swapfile bs=1M count=$SWAP_MB status=progress
     chmod 0600 /mnt/swapfile
     mkswap /mnt/swapfile
     swapon /mnt/swapfile
 fi
 
-# --- Package Installation ---
+# Basestrap
 BASE_PKGS="base base-devel linux linux-firmware intel-ucode amd-ucode dinit elogind-dinit dbus-dinit doas vi networkmanager networkmanager-dinit grub efibootmgr ntfs-3g dosfstools mtools whiptail"
 AUDIO_PKGS="pipewire pipewire-alsa pipewire-pulse wireplumber alsa-utils pavucontrol"
 FS_PKGS=""
 [[ "$FS_CHOICE" == "btrfs" ]] && FS_PKGS="btrfs-progs"
 [[ "$FS_CHOICE" == "xfs" ]] && FS_PKGS="xfsprogs"
 [[ "$FS_CHOICE" == "f2fs" ]] && FS_PKGS="f2fs-tools"
+SWAP_PKGS="zramen zramen-dinit"
 
-basestrap /mnt $BASE_PKGS $AUDIO_PKGS $FS_PKGS
+basestrap /mnt $BASE_PKGS $AUDIO_PKGS $FS_PKGS $SWAP_PKGS
 
 fstabgen -U /mnt >> /mnt/etc/fstab
-[[ "$SWAPSIZE" != "0" ]] && echo "/swapfile none swap defaults 0 0" >> /mnt/etc/fstab
+[[ "$SWAP_MB" -gt 0 ]] && echo "/swapfile none swap defaults 0 0" >> /mnt/etc/fstab
 
-# --- Localization & Timezone ---
-LOCALE=$(whiptail --title "$TITLE" --menu "Select locale:" 20 70 10 \
+# DE/WM selection
+DESKTOP_TYPE=$(whiptail --title "$TITLE" --menu "Select Desktop or Window Manager" 20 70 6 \
+"DE" "Desktop Environment" \
+"WM" "Window Manager Only" \
+3>&1 1>&2 2>&3)
+
+[ -z "$DESKTOP_TYPE" ] && exit 1
+
+if [[ "$DESKTOP_TYPE" == "DE" ]]; then
+    DE_CHOICE=$(whiptail --title "$TITLE" --menu "Select Desktop Environment" 20 70 6 \
+    "Plasma" "KDE Plasma Full Suite" \
+    "XFCE" "XFCE4 + Goodies" \
+    "MATE" "MATE Desktop + Extra" \
+    "LXQt" "LXQt Desktop" \
+    "Moksha" "Moksha Desktop (Enlightenment fork)" 3>&1 1>&2 2>&3)
+elif [[ "$DESKTOP_TYPE" == "WM" ]]; then
+    DE_CHOICE=$(whiptail --title "$TITLE" --menu "Select Window Manager" 20 70 6 \
+    "i3" "i3 Window Manager" \
+    "XMonad" "XMonad Window Manager" \
+    "WindowMaker" "WindowMaker WM" 3>&1 1>&2 2>&3)
+fi
+
+# Localization
+LOCALE=$(whiptail --title "$TITLE" --menu "Select locale" 20 70 10 \
 $(grep "UTF-8" /mnt/usr/share/i18n/SUPPORTED | awk '{print $1 " " $1}') 3>&1 1>&2 2>&3)
-validate_input "$LOCALE"
+LOCALE=$(validate_input "$LOCALE")
 echo "$LOCALE UTF-8" >> /mnt/etc/locale.gen
 artix-chroot /mnt locale-gen
 echo "LANG=$LOCALE" > /mnt/etc/locale.conf
 
-TIMEZONE=$(whiptail --title "$TITLE" --menu "Select timezone:" 20 70 10 \
+TIMEZONE=$(whiptail --title "$TITLE" --menu "Select timezone" 20 70 10 \
 $(awk '/^[^#]/ {print $3 " " $3}' /mnt/usr/share/zoneinfo/zone.tab | sort) 3>&1 1>&2 2>&3)
-validate_input "$TIMEZONE"
+TIMEZONE=$(validate_input "$TIMEZONE")
 artix-chroot /mnt ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
 artix-chroot /mnt hwclock --systohc
 
-# --- Hostname & Users ---
+# Hostname & User
+HOSTNAME=$(whiptail --title "$TITLE" --inputbox "Enter hostname" 10 60 artix 3>&1 1>&2 2>&3)
+HOSTNAME=$(validate_input "$HOSTNAME")
 echo "$HOSTNAME" > /mnt/etc/hostname
+
+ROOT_PW=$(get_password "Enter Root Password")
 echo "root:$ROOT_PW" | artix-chroot /mnt chpasswd
+
+USERNAME=$(whiptail --title "$TITLE" --inputbox "Enter username" 10 60 user 3>&1 1>&2 2>&3)
+USERNAME=$(validate_input "$USERNAME")
+USER_PW=$(get_password "Enter password for $USERNAME")
 artix-chroot /mnt useradd -m -G wheel,audio,video,storage "$USERNAME"
 echo "$USERNAME:$USER_PW" | artix-chroot /mnt chpasswd
+
+# DOAS
 echo "permit persist :wheel" > /mnt/etc/doas.conf
 artix-chroot /mnt chown root:root /etc/doas.conf
 artix-chroot /mnt chmod 0400 /etc/doas.conf
 
-# --- DE/WM Installation ---
-if [[ "$UI_TYPE" == "DE" ]]; then
-    case $DE_CHOICE in
-        Plasma) artix-chroot /mnt pacman -S --noconfirm plasma kde-applications sddm-dinit ;;
-        XFCE)   artix-chroot /mnt pacman -S --noconfirm xfce4 xfce4-goodies lightdm-dinit ;;
-        MATE)   artix-chroot /mnt pacman -S --noconfirm mate mate-extra system-config-printer blueman connman-gtk lightdm-dinit ;;
-        LXQt)   artix-chroot /mnt pacman -S --noconfirm lxqt sddm-dinit ;;
-        Moksha) artix-chroot /mnt pacman -S --noconfirm moksha-artix lightdm-dinit ;;
-    esac
-elif [[ "$UI_TYPE" == "WM" ]]; then
-    case $WM_CHOICE in
-        i3) artix-chroot /mnt pacman -S --noconfirm i3-gaps i3status dmenu ;;
-        xmonad) artix-chroot /mnt pacman -S --noconfirm xmonad xmonad-contrib xmobar ;;
-        WindowMaker) artix-chroot /mnt pacman -S --noconfirm windowmaker wmaker-tools ;;
-    esac
-fi
+# Install chosen DE/WM
+case $DE_CHOICE in
+    Plasma) artix-chroot /mnt pacman -S --noconfirm plasma kde-applications sddm-dinit ;;
+    XFCE)   artix-chroot /mnt pacman -S --noconfirm xfce4 xfce4-goodies lightdm-dinit ;;
+    MATE)   artix-chroot /mnt pacman -S --noconfirm mate mate-extra lightdm-dinit ;;
+    LXQt)   artix-chroot /mnt pacman -S --noconfirm lxqt sddm-dinit ;;
+    Moksha) artix-chroot /mnt pacman -S --noconfirm moksha-artix lightdm-dinit ;;
+    i3)     artix-chroot /mnt pacman -S --noconfirm i3 dmenu lightdm-dinit ;;
+    XMonad) artix-chroot /mnt pacman -S --noconfirm xmonad xmobar lightdm-dinit ;;
+    WindowMaker) artix-chroot /mnt pacman -S --noconfirm windowmaker lightdm-dinit ;;
+esac
 
-# --- Enable Services & Bootloader ---
+# Enable services
 artix-chroot /mnt mkdir -p /etc/dinit.d/boot.d
-for svc in dbus NetworkManager elogind zramen sddm lightdm; do
+for svc in dbus NetworkManager elogind zramen; do
     [ -f "/mnt/etc/dinit.d/$svc" ] && artix-chroot /mnt ln -sf /etc/dinit.d/$svc /etc/dinit.d/boot.d/
 done
 
+[[ "$DE_CHOICE" == "Plasma" || "$DE_CHOICE" == "LXQt" ]] && DM="sddm"
+[[ "$DE_CHOICE" == "XFCE" || "$DE_CHOICE" == "MATE" || "$DE_CHOICE" == "Moksha" || "$DE_CHOICE" == "i3" || "$DE_CHOICE" == "XMonad" || "$DE_CHOICE" == "WindowMaker" ]] && DM="lightdm"
+
+[ -f "/mnt/etc/dinit.d/$DM" ] && artix-chroot /mnt ln -sf /etc/dinit.d/$DM /etc/dinit.d/boot.d/
+
+# Bootloader
 artix-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Artix
 artix-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
-# --- Finish ---
+# Finish
 umount -R /mnt
 sync
 
@@ -197,7 +210,7 @@ if whiptail --title "$TITLE" --yesno "Installation complete! Reboot now?" 10 60;
     reboot
 else
     clear
-    echo "Installation finished. Still in live environment."
-    echo "Chroot into new system with: artix-chroot /mnt"
-    echo "Reboot manually when ready."
+    echo "Installation finished. You are still in the live environment."
+    echo "To chroot: artix-chroot /mnt"
+    echo "Then 'reboot' when ready."
 fi
